852300d513dacea106c2e1f689c2be7a7d9d8e44
jmachinadev <jmachinadev@gmail.com>
2021-03-31 10:47:16 -0400

    add imgur wiring tutorial

--- src/components/wiring/index.tsx
+++ src/components/wiring/index.tsx
@@ -1,8 +1,28 @@
+import React, { useEffect } from "react";
 import React from "react";
 
+
 const WiringTutorialComponent = () => {
+
+  useEffect( () => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = "//s.imgur.com/min/embed.js";
+    document.body.appendChild(script);
+  }, []);
+
   return (
-    <div>Wiring Tutorial</div>
+    <main className="wiring-component">    
+      <article>
+        <header>
+          <h2>Wiring Tutorial</h2>
+          <p>The Oh-Keycaps way...</p>
+        </header>
+        <blockquote className="imgur-embed-pub" lang="en" data-id="a/H2j1yIw"  >
+          <a href="//imgur.com/a/H2j1yIw">Dactyl Manuform building tips</a>
+        </blockquote>
+    </article>
+    </main>
   );
 };
 
